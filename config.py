8d345640fb44f41674a62ec4335cbad203e4bdb0
Wieneber Macias <wieneber76@gmail.com>
2024-09-07 20:21:15 -0600

    Se define form y configuracion  SQL

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+class Config:
+    DB_USERNAME = 'root'
+    DB_PASSWORD = ''
+    DB_HOST = 'localhost'
+    DB_NAME = 'db_users'
---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, DateField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=100)])
+    apellido_paterno = StringField('Apellido Paterno', validators=[DataRequired(), Length(min=2, max=100)])
+    apellido_materno = StringField('Apellido Materno', validators=[DataRequired(), Length(min=2, max=100)])
+    fecha_nacimiento = DateField('Fecha de Nacimiento', validators=[DataRequired()])
+    estado = StringField('Estado', validators=[DataRequired(), Length(min=2, max=100)])
+    nickname = StringField('Nickname', validators=[DataRequired(), Length(min=2, max=100)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Registrarse')
