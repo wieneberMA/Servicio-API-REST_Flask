72c80276fdf28ab0aca3841947fef4b7b508c359
Wieneber Macias <wieneber76@gmail.com>
2024-09-07 20:21:31 -0600

    Se agregan Complementos

---
+++ models.py
@@ -0,0 +1,26 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido_paterno = db.Column(db.String(100), nullable=False)
+    apellido_materno = db.Column(db.String(100), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+    estado = db.Column(db.String(100), nullable=False)
+    nickname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __init__(self, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, estado, nickname, email, password):
+        self.nombre = nombre
+        self.apellido_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.fecha_nacimiento = fecha_nacimiento
+        self.estado = estado
+        self.nickname = nickname
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.nombre
