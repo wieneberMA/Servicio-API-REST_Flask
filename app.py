2e4280bd617ff16819206e73e00fb205af96e2b5
Wieneber Macias <wieneber76@gmail.com>
2024-09-08 23:14:45 -0600

    Se define logica de salarios y vista salarios

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager
+from flask import jsonify
+from flask_login import LoginManager, UserMixin, login_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-from flask_jwt_extended import create_access_token, jwt_required, JWTManager
-from flask import jsonify
+# archivos
+from flask import send_file
+import os
+import csv
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}/{Config.DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['SECRET_KEY'] = '1q2w3e4r5t6y7u8i9o0p'
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'Archivos')
+ALLOWED_EXTENSIONS = {'csv'}
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 from models import User
 
 login_manager = LoginManager(app)
@@ -91,3 +97,45 @@ def show_signup_form():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template("form_registro.html", form=form)
+
+
+@app.route("/salarios")
+def show_salarios():
+    return render_template("carga_salarios.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            extension = filename.split('.')[-1].lower()
+            if extension in ALLOWED_EXTENSIONS:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), newline='') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',', quotechar='"')  # Utiliza la coma como delimitador
+                    next(reader)  # Saltarte la primera fila del archivo CSV
+                    datos = [row for row in reader]  # Asegúrate de que datos sea una lista de listas
+
+                    # Procesar los datos para calcular el salario promedio y el empleado con el salario más alto
+                    salarios = []
+                    for row in datos:
+                        if len(row) > 1:  # Asegúrate de que la fila tenga al menos dos columnas
+                            nombre = row[0]
+                            salario = float(row[1])  # Asumimos que el salario es el segundo elemento de la fila
+                            salarios.append((nombre, salario))
+
+                    if salarios:
+                        salario_promedio = sum(salario for _, salario in salarios) / len(salarios)
+                        empleado_mas_alto = max(salarios, key=lambda x: x[1])
+
+                        msj = 'Archivo cargado'
+                        return render_template("carga_salarios.html", msj=msj, Data=datos, salario_promedio=salario_promedio, empleado_mas_alto=empleado_mas_alto)
+                    else:
+                        msj = 'No se encontraron datos válidos en el archivo'
+                        return render_template("carga_salarios.html", msj=msj, Data=datos)
+            else:
+                return 'La extensión del archivo no está permitida', 400
+        else:
+            return 'No se ha seleccionado ningún archivo', 400
